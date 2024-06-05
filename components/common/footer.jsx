f4156ccb427d6ffa91967196f9cae56b25c20049
Tran Xuan Son <tranxuanson98@gmail.com>
2024-06-05 23:51:41 +0700

    update phone button

--- components/common/footer.jsx
+++ components/common/footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { emailIcon, tgIcon, xIcon } from "../../constant/svg/index";
+import { emailIcon, phoneIcon, tgIcon, xIcon } from "../../constant/svg/index";
 import Link from "next/link";
 
 function Footer() {
@@ -32,6 +32,17 @@ function Footer() {
       {tgIcon}
     </Link>
   );
+
+  const phone = (
+    <Link
+      href="tel:0856599797"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {phoneIcon}
+    </Link>
+  );
+
   return (
     <footer>
       <div class="fixed bottom-[15px] md:bottom-[50px]  right-4 md:right-[55px] z-50">
@@ -47,6 +58,11 @@ function Footer() {
       <div class="fixed bottom-[125px] md:bottom-[180px] right-4 md:right-[55px] z-50">
         <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-4 rounded-full shadow-lg w-[60px] flex justify-center">
           {telegram}
+        </button>
+      </div>
+      <div class="fixed bottom-[180px] md:bottom-[245px] right-4 md:right-[55px] z-50">
+        <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-4 rounded-full shadow-lg w-[60px] flex justify-center">
+          {phone}
         </button>
       </div>
       <div className="container footer">
